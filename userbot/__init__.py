3ac0fd04e2f4e1bf93828de79eb0bce2a106290f
DhimasArbi <dhimas.arbi@gmail.com>
2020-04-01 10:40:18 +0700

    init: add upstream repo branch

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -79,7 +79,11 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/MoveAngel/One4uBot.git")
+    "https://github.com/DhimasArbi/D-bot.git")
+
+# UPSTREAM_REPO_URL branch, the default is master
+UPSTREAM_REPO_BRANCH = os.environ.get(
+    "UPSTREAM_REPO_BRANCH", "sql-extended")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
